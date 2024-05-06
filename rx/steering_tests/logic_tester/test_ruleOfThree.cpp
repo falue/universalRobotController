7872b22c46ee4696d38b82a04bc4f42e4a578cd2
falue <info@fluescher.ch>
2024-05-06 22:45:37 +0200

    Add test for ruleOfThree function

---
+++ rx/steering_tests/logic_tester/test_ruleOfThree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm> // For std::max and std::min
+#include <cmath>    // Include the cmath library for atan2 and constants
+
+// Test like this:
+//   g++ -o test_ruleOfThree test_ruleOfThree.cpp && ./test_ruleOfThree 
+
+int ruleOfThree(int a, int b, int c) {
+  //
+  if (b == 0) { // Check for division by zero
+      return 0; // Return zero or an appropriate error value
+  }
+  int d = (c * b) / a;
+  return d;
+}
+
+int main() {
+    // Test cases
+    std::cout << "Testing:" << std::endl;
+
+    std::cout << "ruleOfThree(75, 100, 50) = "
+              << ruleOfThree(75, 100, 50) << std::endl;
+}
